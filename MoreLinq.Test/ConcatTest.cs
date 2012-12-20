34e19d4b9bb90e11b5396ced5c78ef1a251f8b29
azizatif <azizatif@localhost>
2012-12-20 08:32:29 +0100

    PUSHD stack leak in build batch script

---
+++ MoreLinq.Test/ConcatTest.cs
@@ -0,0 +1,115 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008-2011 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MoreLinq.Test
+{
+    [TestFixture]
+    public class ConcatTest
+    {
+        #region Concat with single head and tail sequence
+
+        // NOTE: Concat with single head and tail sequence is now 
+        // implemented in terms of Prepend so the tests are identical. 
+
+        [Test]
+        public void ConcatWithNonEmptyTailSequence()
+        {
+            string[] tail = { "second", "third" };
+            string head = "first";
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual("first", "second", "third");
+        }
+
+        [Test]
+        public void ConcatWithEmptyTailSequence()
+        {
+            string[] tail = { };
+            string head = "first";
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual("first");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatWithNullTailSequence()
+        {
+            MoreEnumerable.Concat("head", null);
+        }
+
+        [Test]
+        public void ConcatWithNullHead()
+        {
+            string[] tail = { "second", "third" };
+            string head = null;
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual(null, "second", "third");
+        }
+
+        [Test]
+        public void ConcatIsLazyInTailSequence()
+        {
+            MoreEnumerable.Concat("head", new BreakingSequence<string>());
+        }
+        #endregion
+
+        #region Concat with single head and tail sequence
+        [Test]
+        public void ConcatWithNonEmptyHeadSequence()
+        {
+            string[] head = { "first", "second" };
+            string tail = "third";
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual("first", "second", "third");
+        }
+
+        [Test]
+        public void ConcatWithEmptyHeadSequence()
+        {
+            string[] head = { };
+            string tail = "first";
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual("first");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatWithNullHeadSequence()
+        {
+            MoreEnumerable.Concat(null, "tail");
+        }
+
+        [Test]
+        public void ConcatWithNullTail()
+        {
+            string[] head = { "first", "second" };
+            string tail = null;
+            IEnumerable<string> whole = MoreEnumerable.Concat(head, tail);
+            whole.AssertSequenceEqual("first", "second", null);
+        }
+
+        [Test]
+        public void ConcatIsLazyInHeadSequence()
+        {
+            MoreEnumerable.Concat(new BreakingSequence<string>(), "tail");
+        }
+        #endregion
+    }
+}
---
+++ MoreLinq.Test/PadTest.cs
@@ -0,0 +1,87 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008-2011 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using NUnit.Framework;
+
+namespace MoreLinq.Test
+{
+    [TestFixture]
+    public class PadTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PadNullSource()
+        {
+            MoreEnumerable.Pad<object>(null, 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PadNegativeWidth()
+        {
+            MoreEnumerable.Pad(new object[0], -1);
+        }
+
+        [Test]
+        public void PadIsLazy()
+        {
+            MoreEnumerable.Pad(new BreakingSequence<object>(), 0);
+        }
+
+        [Test]
+        public void PadWithFillerIsLazy()
+        {
+            MoreEnumerable.Pad(new BreakingSequence<object>(), 0, new object());
+        }
+
+        [Test]
+        public void PadWideSourceSequence()
+        {
+            var result = MoreEnumerable.Pad(new[] { 123, 456, 789 }, 2);
+            result.AssertSequenceEqual(new[] { 123, 456, 789 });
+        }
+
+        [Test]
+        public void PadEqualSourceSequence()
+        {
+            var result = MoreEnumerable.Pad(new[] { 123, 456, 789 }, 3);
+            result.AssertSequenceEqual(new[] { 123, 456, 789 });
+        }
+
+        [Test]
+        public void PadNarrowSourceSequenceWithDefaultPadding()
+        {
+            var result = MoreEnumerable.Pad(new[] { 123, 456, 789 }, 5);
+            result.AssertSequenceEqual(new[] { 123, 456, 789, 0, 0 });
+        }
+
+        [Test]
+        public void PadNarrowSourceSequenceWithNonDefaultPadding()
+        {
+            var result = MoreEnumerable.Pad(new[] { 123, 456, 789 }, 5, -1);
+            result.AssertSequenceEqual(new[] { 123, 456, 789, -1, -1 });
+        }
+
+        [Test]
+        public void PadNarrowSourceSequenceWithDynamicPadding()
+        {
+            var result = MoreEnumerable.Pad("hello".ToCharArray(), 15, i => i % 2 == 0 ? '+' : '-');
+            result.AssertSequenceEqual("hello-+-+-+-+-+".ToCharArray());
+        }
+    }
+}
